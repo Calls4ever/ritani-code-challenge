d000c5874ba8ff0febd3088c7778b92401af0faa
callas4ever <thetseringnorbu@gmail.com>
2021-05-19 14:14:01 -0400

    final setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import './App.css';
-// import {AiOutlineSearch} from 'react-icons/ai';
-// import {BsSearch} from 'react-icons/bs';
-import { FaBeer } from 'react-icons/fa';
+import {BsMoon} from 'react-icons/bs'
+import {BsSun} from 'react-icons/bs'
+import {useState} from 'react'
+
 function App() {
-  const searchBar = <FaBeer size ={42}/>
+  const [lightTheme, setLightTheme]=useState(true)
+  
+  if(lightTheme){
+    document.body.style.backgroundColor="white"
+    document.body.style.color='black'
+  }else{
+    document.body.style.backgroundColor="black"
+    document.body.style.color='white'
+  }
+  console.log(document.body.style)
   return (
-    <div className="main">
+    <div>
       <div className='dark-mode-toggle-div'>
-        
+      <button 
+      className = 'theme-mode-button'
+      onClick={()=>setLightTheme(true)}
+      
+      >
+          <BsSun 
+          size ={35}
+          color = "white"
+          />
+        </button>
+        <button 
+        className = "theme-mode-button"
+        onClick = {()=>setLightTheme(false)}
+        >
+          <BsMoon 
+          size={35}
+          color="black"
+          />
+        </button>
       </div>
-      <div className='search-input-div'>
+      <div className={lightTheme?'search-input-div':'search-input-div dark'}>
         <form>
           <input 
-          className = 'search-input'
+          className = {lightTheme?'search-input':'search-input dark'}
           placeholder= '@example'
           />
-          <i>
-            {searchBar}
-          </i>
         </form>
         
       </div>
