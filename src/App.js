32a7672b0eb6dc5cdef3d2a825fc599c920f7f0b
callas4ever <thetseringnorbu@gmail.com>
2021-05-21 12:48:05 -0400

    commenting my code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
 import HomePage from './components/HomePage';
+//This app consist of 5 components 
+// HomePge-a container for all other components
+//UserPage - Renders User details and FollowerList Component
+//FollowerList - renders details of each follower
+//Loading - displays the loading animation while the data is being loaded from the api
+//NoUserFound - displays no user found only when there were no user with given username
+
+//Used React icons
+//Used React hooks
+
 
 function App() {
-  
   return (
     <div>
       <HomePage/>
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,20 @@
+//importing icons for dark and light mode
 import {BsMoon} from 'react-icons/bs'
 import {BsSun} from 'react-icons/bs'
-import {useState, useEffect} from 'react'
+//Components being imported
 import UserPage from './UserPage'
 import Loading from './Loading'
 import NoUserFound from './NoUserFound'
+import {useState} from 'react'
 
 const HomePage=(props)=> {
+  //States
+  //there are 5 states in this components
+  //lightThem - Tracks what theme (light & dark) user is on
+  //searchKey - Tracks user's input
+  //notFound -  Decides when to render NoUserFound component
+  //loading - Decides when to rending Loading Component
+  //currentUser - Stores current user's info
   const [lightTheme, setLightTheme]=useState(true)
   const [searchKey, setSearchKey]=useState('')
   const [notFound, setNotFound] =useState(false)
@@ -18,7 +27,7 @@ const HomePage=(props)=> {
 })
 
     
-
+//Rendering theme
   if(lightTheme){
     document.body.style.backgroundColor="white"
     document.body.style.color='black'
@@ -26,9 +35,17 @@ const HomePage=(props)=> {
     document.body.style.backgroundColor="black"
     document.body.style.color='white'
   }
+
+  //Handle change function to track what is being type in the search box
   const handleChange = e =>{
-     setSearchKey(e.target.value)
+     setSearchKey(e.target.value.trim())
   }
+  //on submitting the search query, couple key things expected to happen
+  // 1. Sets current user if any to null 
+  // 2. Starts loading
+  // 3. Fetches user from API
+  // 4. Empties the search box
+
   const handleSubmit = e =>{
       e.preventDefault()
       setCurrentUser({
@@ -42,6 +59,12 @@ const HomePage=(props)=> {
       setSearchKey("")
   }
 
+  //FetchUser
+  //it is an sync function that fetches user from the API
+  //it should do following key things
+  //1. fetches user with given username
+  //2. if there is user, then start fetching user's followers
+  //3. if not, the resets NotFound to true
   const  fetchUser = async (keyword) =>{
     
     let user = await fetch(`https://api.github.com/users/${keyword}`)
@@ -61,17 +84,30 @@ const HomePage=(props)=> {
     }
     
   }
+
+//adjusting the length (width) of the search input when it is on focus or out of focus
 const handleOnFocus=e=>{
     e.target.style='width: 580px;'
 }
 const handleOnBlur=e=>{
     e.target.style='width: 360px;'
 }
+
+//fetchFollowers
+//it is an async function that fetches followers of given user from API
+//Following key things expected to do
+//1. Check if the user it is given matches user in the currentUser
+//2. if yes, copies all the info from currentUser to a variable (data)
+//3. if not, sets the value of data to initial (default) current user's values
+//4. gets followers from the api using correct page number stored in data
+//5. Check if there is more followers left to get
+//6. if yes, increment the page number for future uses
+//7. if not, set morePage property to false
+//8. at the end, it updates loading, currentUser, and notFound
 const fetchFollower =async (user)=>{
     let data={}
     if(currentUser.user && user.login==currentUser.user.login){
         data=currentUser
-        console.log(data)
     }else{
        data = {
             user,
@@ -100,29 +136,32 @@ const fetchFollower =async (user)=>{
         setCurrentUser(data)
         setNotFound(false)
 }
-
+//Here renders the components we wanted to see
+//first div is wrapper div of all other JSX
+//div with class 'dark-mode-toggle-div' holds both of the icons for dark and light mode
+//then comes form with search input box
+//then conditionally renders NoUserFound, Loading and UserPage components
   return (
     <div>
       <div className='dark-mode-toggle-div'>
-      <button 
-      className = 'theme-mode-button'
-      onClick={()=>setLightTheme(true)}
-      
-      >
+        <button 
+        className = 'theme-mode-button'
+        onClick={()=>setLightTheme(true)}
+        >
           <BsSun 
-          size ={35}
-          color = "white"
-          />
+            size ={35}
+            color = "white"
+            />
         </button>
         <button 
-        className = "theme-mode-button"
-        onClick = {()=>setLightTheme(false)}
+          className = "theme-mode-button"
+          onClick = {()=>setLightTheme(false)}
         >
           <BsMoon 
-          size={35}
-          color="black"
-          />
-          
+            size={35}
+            color="black"
+            />
+            
         </button>
       </div>
       <div className={lightTheme?'search-input-div':'search-input-div dark'}>
