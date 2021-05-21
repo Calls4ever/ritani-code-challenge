74ac3ee2bdec4269727f0c462b6b5050b2bae8ba
callas4ever <thetseringnorbu@gmail.com>
2021-05-21 14:26:46 -0400

    commenting my code

--- src/components/NoUserFound.js
+++ src/components/NoUserFound.js
@@ -1,4 +1,5 @@
 const NoUserFound = props=>{
+    //just a very plain 404 landing page even if we don't know the reason for failing is actually 404 LOL
     return(
         <div className = 'not-found-div'>
             <h1 className = 'four-o-four'>404</h1>
--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -1,17 +1,20 @@
-import { useState } from 'react'
 import FollowerList from './FollowerList'
-import Loading from './Loading'
 
 const UserPage = (props)=>{
+    //fetches more followers if exist when clicking on the load more button
     const handleLoadMoreClick=()=>{
         props.fetchFollower(props.user.user)
     }
+    //There are few divs, many of them structured in this way for styling purposes
+    //The div with class name of 'user-container' holds all the element in this component
+    //There are three main parts, the div containing user details, follower list and conditional render of load more button
+    //The user details div divided into one <section>, one <h1/> and one <p/>
+    //The section in the user detail contains user details like nam, username etc..
     return(
         <div className = 'user-container'>
             <div className ={props.theme?'user-details-div':'user-details-div-dark'}>
                <section className = 'user-details-section'>
                    <img 
-
                    src = {props.user.user.avatar_url}
                    alt="user's profile"
                    className = 'user-avatar'
